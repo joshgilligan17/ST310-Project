444acca552bb0412c7e16dfd2c6cfcd172e250d7
joshgilligan17 <74508367+joshgilligan17@users.noreply.github.com>
2021-12-18 13:35:35 +0000

    Update Part1.R

--- Part1.R
+++ Part1.R
@@ -1,13 +1,19 @@
 #Data Cleaning and Balancing
 library(tidyverse)
 
-set.seed(42)
+set.seed(80)
 #Split male and female patients
-maleSet <- filter(heart,Sex=="M") %>%
-  sample_n(size=193)
+
+heart <- filter(heart,heart$Cholesterol!=0)
 femaleSet <- filter(heart,Sex=="F")
+maleSet <- filter(heart,Sex=="M") %>%
+  sample_n(size=nrow(femaleSet))
+
 #Create balanced sets with 193 from each gender
 balancedSet <- rbind(maleSet,femaleSet)
+balancedSet$HeartDisease <- ifelse(balancedSet$HeartDisease == 1,"Yes","No")
+balancedSet$HeartDisease <- as.factor(balancedSet$HeartDisease)
+
 
 #Splitting into train and test sets
 split<- sort(sample(nrow(balancedSet), nrow(balancedSet)*.8))
@@ -15,7 +21,7 @@ train <- balancedSet[split,]
 test <- balancedSet[-split,]
 
 #Checking the imbalanced in test and train heart disease
-mean(test$HeartDisease)
-mean(train$HeartDisease)
+sum(ifelse(test$HeartDisease=="Yes",1,0))/nrow(test)
+sum(ifelse(train$HeartDisease=="Yes",1,0))/nrow(train)
 
 
